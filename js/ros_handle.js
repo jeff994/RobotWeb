02ab12be8b68fa702c8e3aabe28b674422d832fe
jeff994 <hujianbo1981@gmai.com>
2017-01-13 13:47:23 +0800

    start to separate js files

---
+++ js/ros_handle.js
@@ -0,0 +1,33 @@
+ var ros = new ROSLIB.Ros({
+    url : 'ws://192.168.23.62:9090'
+  });
+
+ros.on('connection', function() {
+    console.log('Connected to websocket server.');
+  });
+
+  ros.on('error', function(error) {
+    console.log('Error connecting to websocket server: ', error);
+  });
+
+  ros.on('close', function() {
+    console.log('Connection to websocket server closed.');
+  });
+
+  var listener = new ROSLIB.Topic({
+    ros : ros,
+    name : '/status',
+    messageType : 'std_msgs/String'
+  });
+
+ var publisher = new ROSLIB.Topic({
+        ros : ros,
+        name : '/keyboard',
+        messageType : 'std_msgs/String'
+      });
+ 
+  function publish(str)
+  {
+    var twist = new ROSLIB.Message({data : str})
+    publisher.publish(twist);
+  }
